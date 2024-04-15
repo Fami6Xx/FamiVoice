f86fcd1808f2045fed1536370d5ce450434b1363
Fami6Xx <libojer@gmail.com>
2024-04-15 20:25:06 +0100

    feat: init

---
+++ src/main/java/org/permadoor/famivoice/FamiVoice.java
@@ -0,0 +1,17 @@
+package org.permadoor.famivoice;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FamiVoice extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
